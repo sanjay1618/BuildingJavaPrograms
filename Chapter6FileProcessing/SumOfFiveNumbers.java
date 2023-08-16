9ecb57fe3a456800d0ad0c422c268561e11c9017
sanjay <sanjay.uppugandla@gmail.com>
2023-08-16 09:57:08 -0700

    Sum of Five Numbers.java

--- Chapter6FileProcessing/SumOfFiveNumbers.java
+++ Chapter6FileProcessing/SumOfFiveNumbers.java
@@ -1,5 +1,14 @@
 
-
+import java.io.*;
+import java.util.*;
 public class SumOfFiveNumbers {
-    
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner input =new Scanner(new File("TextFileWithNumbers.txt"));
+        double sum=0.0;
+        while(input.hasNextDouble()){
+            sum=sum+input.nextDouble();
+        }
+        System.out.println("The Sum of numbers in the files are :"+sum);
+    }
+
 }
