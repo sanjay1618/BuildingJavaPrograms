dd7b415fc252faa813120460d0bb9964d8fe91e3
sanjay <sanjay.uppugandla@gmail.com>
2023-08-17 15:51:16 -0700

    complete program updated

--- CaseStudy_Stock/StockMain.java
+++ CaseStudy_Stock/StockMain.java
@@ -1,12 +1,22 @@
 import java.util.*;
-import java.io.*;
 public class StockMain {
     public static void main(String[] args) {
         Scanner console= new Scanner(System.in);
         System.out.println("First stock's symbol");
         String symbol1= console.next();
         Stock stock1= new Stock(symbol1);
-        double profit1= 
+        double profit1=makePurchases(stock1, console);
+        System.out.println("Second Stock's Symbol");
+        String symbol2= console.next();
+        Stock stock2= new Stock(symbol2);
+        double profit2= makePurchases(stock2, console);
+
+        if(profit1>profit2){
+            System.out.println(profit1-profit2 + " stock1's profit is more");
+        }
+        else{
+            System.out.println("stock2 is profit bigger by"+ (profit2-profit1));
+        }
         
     }
     
@@ -21,6 +31,14 @@ public class StockMain {
             //recording in the stock object and calling the purchase method.
             currentStock.purchase(numShares,pricePerShare);
         }
-        System.out.println()
+        System.out.println("What is the current price");
+        int currentPrice=console.nextInt();
+        double profit=currentStock.getProfit(currentPrice);
+        return profit;
+
+
+
+
+
     }
 }
